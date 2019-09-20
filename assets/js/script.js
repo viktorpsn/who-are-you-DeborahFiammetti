d05516884f4ad0c2a35661f2e2dafdd8dedbf2fb
Deborah Fiammetti <fiammetti1@vafl-o-d02-02.unilim.fr>
2019-09-20 09:13:59 +0200

    version final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,25 @@
 // Your script goes here
-console.log("Hello World");
+
+var section_ratio_visible = 0.55;
+var sections = [
+	document.querySelector('#perso'),
+	document.querySelector('#pro'),
+];
+
+var observer = new IntersectionObserver( function(entries) {
+	entries.forEach(function(entry) {
+		var id = entry.target.getAttribute('id');
+		document.querySelector(`[href="#${id}"]`).parentNode.classList.remove('navigation_active');
+		if(entry.intersectionRatio >= section_ratio_visible) {
+			document.querySelector(`[href="#${id}"]`).parentNode.classList.add('navigation_active');
+		}
+	});
+
+}, {
+	threshold: section_ratio_visible
+});
+
+
+sections.forEach( function(section) {
+	observer.observe(section);
+});
